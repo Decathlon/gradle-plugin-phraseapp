87758c83501a46c7eacf7fc612645762aa5ea540
adrienbusin <adrien.busin@decathlon.com>
2023-07-25 16:29:09 +0200

    refactor: split CheckTask to two distinct tasks.
    * Create PlaceholderCheckTask to check placeholders in other languages.
    * Create PluralsCheckTask to check plurals formatting.

--- client/src/test/kotlin/phraseapp/repositories/checks/PluralsCheckTest.kt
+++ client/src/test/kotlin/phraseapp/repositories/checks/PluralsCheckTest.kt
@@ -1,21 +1,20 @@
-package phraseapp.repositories.checks.strategies
+package phraseapp.repositories.checks
 
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert.assertEquals
 import org.junit.Test
 import phraseapp.extensions.parse
 import phraseapp.internal.platforms.Android
-import phraseapp.repositories.checks.CheckType
+import phraseapp.repositories.checks.strategies.pluralsCheck
 import java.io.File
 
-class PluralsStrategyTest {
+class PluralsCheckTest {
     @Test
     fun shouldDetectWhenThereIsNoAllTranslationsForAPlural() = runBlocking {
         val default = File("src/test/resources/android-errors/values/strings.xml").readText().parse(Android.format)
         val target = File("src/test/resources/android-errors/values-fr-rFR/strings.xml").readText().parse(Android.format)
 
-        val strategy = PluralsStrategy()
-        val errors = strategy.apply(default, target)
+        val errors = default.pluralsCheck(target)
         assertEquals(1, errors.size)
         assertEquals("numberOfSongsAvailable", errors[0].key)
         assertEquals(CheckType.PLURALS, errors[0].type)
@@ -26,8 +25,7 @@ class PluralsStrategyTest {
         val default = File("src/test/resources/android-local/values/strings.xml").readText().parse(Android.format)
         val target = File("src/test/resources/android-local/values-es-rES/strings.xml").readText().parse(Android.format)
 
-        val strategy = PluralsStrategy()
-        val errors = strategy.apply(default, target)
+        val errors = default.pluralsCheck(target)
         assertEquals(0, errors.size)
     }
 }
